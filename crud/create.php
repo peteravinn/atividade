bc6c0ff9ae24f7e6ee9a07da0a08fc23b938e13f
peteravinn <80780644+peteravinn@users.noreply.github.com>
2021-05-29 20:54:02 -0300

    Atv_Av2

---
+++ crud/create.php
@@ -0,0 +1,23 @@
+<?php
+//CONEXÃO E CADASTRO DE ATIVIDADE
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname	= "atividade";
+
+try{
+$conn = new PDO("mysql:host=$servername;dbname=$dbname=charset=utf8", $username, $password,);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$sql = "INSERT INTO dados (Aluno, Disciplina, 'Nota_1', 'Nota_2')
+VALUES ('Andre', 'Web', 10, 10,)";
+// use exec() bacause no results are returned
+$conn->exec($sql); 
+echo "atividade cadastrada com sucesso";
+} catch(PDOException $e) {
+echo" $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
